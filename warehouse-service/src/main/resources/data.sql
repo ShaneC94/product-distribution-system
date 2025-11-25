843153fd383eeb26128338f06641f243cf85440d
Bryan Parmar <bryan_parmar@yahoo.ca>
2025-11-24 19:42:56 -0500

    updated database

--- warehouse-service/src/main/resources/data.sql
+++ warehouse-service/src/main/resources/data.sql
@@ -3,8 +3,22 @@ DELETE FROM products;
 
 INSERT INTO products (product_code, name)
 VALUES
-    (1001, 'Widget A'),
-    (1002, 'Widget B');
+    (1000, 'T-Shirt'),
+    (1001, 'Jeans'),
+    (1002, 'Hoodie'),
+    (1003, 'Jacket'),
+    (1004, 'Sweater'),
+    (1005, 'Dress Shirt'),
+    (1006, 'Polo Shirt'),
+    (1007, 'Shorts'),
+    (1008, 'Sweatpants'),
+    (1009, 'Dress Pants'),
+    (1010, 'Blazer'),
+    (1011, 'Cardigan'),
+    (1012, 'Tank Top'),
+    (1013, 'Skirt'),
+    (1014, 'Dress')
+    ;
 
 INSERT INTO inventories (warehouse_id, product_id, available_quantity, reserved_quantity, updated_at)
 VALUES
